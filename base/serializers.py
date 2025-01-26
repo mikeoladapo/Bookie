4de0b009d60e6b7e39a90813d2769671645f4657
lionheart <mikeoladapo2004@gmail.com>
2025-01-26 14:46:00 +0100

    modified the settings

--- base/serializers.py
+++ base/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework import serializers
+from .models import CustomUser
 
 class CustomUserSerializer(serializers.Serializer):
     username = serializers.CharField(max_length=20)
     email = serializers.EmailField()
     password = serializers.CharField()
+    profile_picture = serializers.ImageField()
     is_active = serializers.BooleanField(default=True)
     is_staff = serializers.BooleanField(default=True)
     is_superuser = serializers.BooleanField(default=False)
-    def create(self,validated_data):
+    def create(self,validated_data): 
+        return CustomUser.objects.create(**validated_data)
+    def update(self,instance,validated_data):
