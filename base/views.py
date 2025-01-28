3cdd9e683a390db35487cae98a9e1f6256149759
lionheart <mikeoladapo2004@gmail.com>
2025-01-28 14:29:34 +0100

    Viewsets

--- base/views.py
+++ base/views.py
@@ -93,3 +93,34 @@ class AuthorViewSet(viewsets.ViewSet):
         user = get_object_or_404(queryset,pk=pk)
         user.delete()
         return Response({"detail": "Author deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+    
+class CategorySerializer(viewsets.ViewSet):
+    def create(self,request):
+        serializer = CategorySerializer(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response (serializer.data , status=status.HTTP_201_CREATED)
+        return Response(serializer.errors ,status=status.HTTP_400_BAD_REQUEST)
+    def list(self,request):
+        queryset = Category.objects.all()
+        serializer = CategorySerializer(queryset,many=True)
+        return Response(serializer.data)
+    def retrieve(self,request,pk=None):
+        queryset = Category.objects.all()
+        user = get_object_or_404(queryset,pk=pk)
+        serializer = CategorySerializer(user)
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    def update(self,request,pk=None):
+        queryset = Category.objects.all()
+        user = get_object_or_404(queryset,pk=pk)
+        serializer = CategorySerializer(user,data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response (serializer.data,status=status.HTTP_200_OK)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def destroy(self,request,pk=None):
+        queryset = Category.objects.all()
+        user = get_object_or_404(queryset,pk=pk)
+        user.delete()
+        return Response({"detail": "Category deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+    
