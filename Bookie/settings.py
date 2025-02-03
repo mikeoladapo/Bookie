4c791b79ef71b2b018b541cf61419259f79869e6
lionheart <mikeoladapo2004@gmail.com>
2025-02-03 15:03:43 +0100

    added database

--- Bookie/settings.py
+++ Bookie/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os 
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -28,7 +29,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
-AUTH_USER_MODEL = 'base.CustomUser'
+#AUTH_USER_MODEL = 'base.CustomUser'
 
 
 # Application definition
@@ -81,10 +82,15 @@ WSGI_APPLICATION = 'Bookie.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'Bookie',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
