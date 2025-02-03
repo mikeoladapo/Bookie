fac6ab66a156948a9392c738df97b4472dcf5431
lionheart <mikeoladapo2004@gmail.com>
2025-02-03 15:26:06 +0100

    added database

--- Bookie/settings.py
+++ Bookie/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Bookie',
         'USER': 'postgres',
-        'PASSWORD': 'postgres',
+        'PASSWORD': 'lionheart',
         'HOST': 'localhost',
         'PORT': '5432',
     }
